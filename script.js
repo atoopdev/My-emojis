23b5c79629f9c89908379bdf4eba96b452440a74
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-01-05 13:14:56 -0800

    Add to End Button
    Add new emoji via input field and render updated array

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 const myEmojis = ["🏃", "📔", "🍳"];
-const emojiContainerEL = document.getElementById("emojicontainer-el");
+let emojiContainerEL = document.getElementById("emojicontainer-el");
+const emojiInput = document.getElementById("emoji-input");
+const addtoEndBtn = document.getElementById("push-btn");
 
 // holds emojis and html
 let listItems = "";
 
+Render();
+
+// ------------------------- Add to End ----------------------------------------
+addtoEndBtn.addEventListener("click", function(){
+    console.log("Add to End button clicked");
+    console.log(emojiInput.value);
+
+    // make sure have input
+    if(emojiInput.value){
+    // add new emoji to array
+    myEmojis.push(emojiInput.value);
+
+    // verify added
+    console.log(myEmojis);
+    Render();
+    } else{
+        console.log("emojiInput.value not truthy");
+    }
+    // clear input field
+    emojiInput.value = "";
+    
+})
+
+// ------------------------------Render --------------------------------------
+
+function Render(){
+    // clear previous versions of render
+    listItems="";
 for(let i=0;i<myEmojis.length; i++){
     console.log(myEmojis[i]);
     listItems += `<span>${myEmojis[i]}</span>`;
 }
 emojiContainerEL.innerHTML = listItems;
+}
